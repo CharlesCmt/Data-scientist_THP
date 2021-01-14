3ced2d4e63afc2c6fc4ebe4ce2f18fe8c14e1b3e
Charles <charlescampet03@gmail.com>
2021-01-14 19:54:39 +0100

    journalists first commit

---
+++ lib/00_journalists.rb
@@ -0,0 +1,125 @@
+#datas
+array = ["@jcunniet","@PaulLampon","@Aziliz31","@ssoumier","@marionsouzeau","@gaellombart","@bendarag","@AurelieLebelle","@julienduffe","@thomaspoupeau","@LilyRossignol","@ClairGuedon","@stephanieauguy","@claw_prolongeau","@_JulieMenard","@LColcomb","@Zlauwereys","@MeLonguet","@DorotheeLN","@NolwennCosson","@ADaboval","@Remibaldy","@bderveaux","@amandechap","@ELODIESOULIE","@nbongarcon","@HeloAb","@Panamorama","@gregplou","@BenoitBerthe","@LauraBruneau89","@Anthony_Lieures","@Sharonwaj","@mcsonkin","@pverduzier","@emiliel3","@Julien_MARION","@SophiFay","@bdelombre","@annalecerf","@AdriaBudry","@DejNikolic","@iJaffre","@CyrusleVirus","@GPardigon","@e_vallerey","@IsabelleSouquet","@AudeDavidRossi","@Yoann_Pa","@CeliaPenavaire","@perraultvincent","@cboulate","@JustineWeyl","@Paulinejacot","@juliechab","@aslechevallier","@phnou","@Seb_Pommier","@Alex_Bensaid","@GuillaumeGaven","@annelaurechouin","@Oliviader","@guerricp","@JMMarchaut","@cyceron","@gregory_raymond","@vhunsinger","@l_peillon","@fannyguinochet","@EAssayag","@KibweAdom","@YvonHerry","@JohanGarciajg","@saidlabidi","@lauranneprov","@LeaDavy","@francois_remy","@CGuicheteau","@FloMaillet","@m_perroud","@oBrunet_TSMF","@MoonVdc","@jc2taille","@NellyMoussu","@VirginK","@b_misa","@FabriceCouste","@barbara_prose","@lelia2m","@brunoaskenazi","@laurenechamp","@ysisbox","@juliengagliardi","@PierreLel","@kdeniau","@_TerraInc","@DominicArpin","@antoinfonteneau","@nanotousch","@jb_roch","@YaniKhezzar","@Anne_Bechet","@NCapart","@SamyBenNaceur","@Joumany","@Julietteraynal","@TGiraudet","@SaraTanit","@HappeFrederic","@antoinellorca","@michelpicot","@Sev_Ryne","@bobdobolino","@murdever","@YGrandmontagne","@Mnyo","@EdKOSCIANSKI","@tnoisette","@jankari","@delbello_rom","@rflechaux","@NadiaSorelli","@IT_Digital","@abarbaux","@PhilippeLeroy","@schaptal","@marionspee","@lisavignoli","@ChloeAeberhardt","@MartineJacot","@JuliaPascualita","@curieusedetout","@sgraveleau","@bif_o","@ElisaPineau","@zinebdryef","@apiquard","@pierrehaski","@StephanieDelmas","@Blandine_Garot","@vergara_i","@evan_lebastard","@SophieVclt","@OlivierLevrault","@alicedorgeval","@LouiseMalnoy","@alix_fx","@pierre_baudis","@LucMagoutier","@AgatheMuller","@SGianninelli","@PaulineBoyer33","@NaomiHalll","@romaindlx","@marionbr","@Burtschy","@JacobEtienne","@as_lizzani","@marie_simon","@LaureDaussy","@FabriceAmedeo","@min","@LoubnaChlaikhy","@PlummerWilliam","@OlivierMarin1","@alaurefremont","@mwesfreid","@ChBaDe","@pmathon","@theobaldmarie","@Lnpagesy","@marclandre","@paoliniesther","@Feertchak","@JBLitzler","@GuillaumeErrard","@quentinperinel","@TristanQM","@mlbo","@constancejamet","@LoraTerrazas","@emiliegeffray","@Mathilde_Sd","@CaroPiquet","@DCanivez","@TIM_7375","@blandinelc","@ivanrioufol","@arthurberdah","@SarahLecoeuvre","@guillaume_gui","@DamienMercereau","@W_Chloe","@Assma_MD","@EugenieBastie","@HiTech_lexpress","@bcondominas","@Laurie_Z_","@jeanfrancgerard","@MathieuPagura","@BGUYF","@AlainPiffaretti","@AudreyKucinskas","@julienhory","@Pierrefalga","@TiphThuillier","@cdaniez","@LigerBaptiste","@D_Peras","@julie_dlb","@Fatiha_Temmouri","@julian2lamancha","@GaetaneDeljurie","@JulianMattei","@M_Vicuna","@DeBruynOlivier","@Nehed_Jendoubi","@antoine_grenapi","@ColonnaGen","@VictoriaGairin","@Clement_Lacombe","@TVigoureux","@MargauxObriot","@solinedelos","@RocheSabine","@dangerkens","@EdouardDutour","@MDondeyne","@DupuisNathalie1","@bouscarel","@Mathieu2jean","@Sophie_T_J","@laurentcalixte","@patrockwilliams","@PascaleKremer","@AlexJaquin","@LauraIsaaz","@cath_robin","@Del_Gautherin","@Isaduriez","@lucietuile","@AugeyBastien","@mcastagnet","@AminaKalache","@mvaudano","@CParrot","@ombelinetips","@_JoinLion","@BarbolosiRose","@ToiBruno1","@FloraClodic","@xjbdx","@ceci_est_un_handle_vraiment_long","@AlexiaEy","@Emjy_STARK","@elcoco01","@rabilebon","@pflovens_","@FabriceFrossard","@MorganeSERRES","@MarjolaineKoch","@edgarsnow","@SRNLF","@CChassigneux","@KerinecMoran","@NassiraELM","@NewsRicard","@Sandreene","@Emilezrt","@Pierre_Do","@Micode","@CColumelli","@DavidAbiker","@ClementBergantz","@benjaminrabier","@celinekallmann","@edwyplenel","@C_Barbier","@JJBourdin_RMC","@LaurenceFerrari","@aslapix","@IsaMillet","@MaximeSwitek","@tomjoubert","@jszanchi","@roqueeva","@XavierBiseul","@florencesantrot","@AntoineCrochet","@freeman59","@MaudeML","@philippe_gulpi","@mathieum76","@kiouari","@imanemoustakir","@BenedicteMallet","@Emilie_Brouze","@antoinebarret","@_nicolasbocquet","@remibuhagiar","@CourretB","@AymericRobert","@miraelmartins","@pmaniere","@jesuisraphk","@David_Ingram","@pcelerier","@technomedia","@Geraldinedauver","@ThierryLabro","@Newsdusud","@nrauline","@gbregeras","@SCouasnonBFM","@actualites_nrv","@dimitrimoulins","@oli_aura","@FabieChiche","@Vincent_Raimblt","@ChristophGreuet","@PAlbuchay","@MarrauddesGrot","@vtalmon","@cedric","@olivierfrigara","@Julien_Jay","@LydiaBerroyer","@Shuua","@datisdaz","@Steuph","@ameliecharnay","@Bruno_LesNums","@LelloucheNico","@CciliaDiQuinzio","@Elodie_C","@SylvRolland","@Kocobe","@FL_Debes","@jdupontcalbo","@GarciaVictor_","@NicoRichaud","@RHoueix","@simottel","@DamienLicata","@annabelle_L","@Lea_Lejeune","@axel_deb","@marin_eben","@ptiberry","@MatthieuDelach","@sandrinecassini","@benjaminferran","@ppgarcia75","@NotPatrick","@ivalerio","@FabienneSchmitt","@alexgoude","@JeromeColombain","@manhack","@Capucine_Cousin","@Fsorel","@oliviertesquet","@marjoriepaillon","@ginades","@PierreTran","@DelphineCuny","@reesmarc","@lauratenoudji","@ldupin","@carolinedescham","@Lucile_Quillet","@cgabizon","@Allocab","@epenser","@JAGventeprivee","@frwrds","@Laure__Bourdon","@Xavier75","@maximeverner","@s_jourdain","@LoriHelloc"]
+
+#Combien y a-t'il de handle dans cette array ?
+def arr_length(array)
+    return array.size
+end
+      
+#Quel est le handle le plus court de cette liste ?
+def smallest_string(array)
+    return array.min_by(&:length)
+end
+
+#Combien y-a-t'il de handle contenant 5 caractères ?(le @ ne compte pas pour un caractère)
+def find_string(arr)
+    count = 0
+    arr.each do |index|
+        if index.length == 6
+            count += 1
+        end
+    end
+    return count
+end
+
+#Combien de handle commencent par une majuscule ? (première lettre juste après le @)
+def is_upcase? (x)
+    ('A'...'Z').cover? x
+end
+
+def find_caps(arr)
+    count = 0
+    arr.each do |index|
+        chars = index.split('')
+        if is_upcase?(chars[1])
+            count += 1
+        end
+    end
+    return count
+end
+
+#Trie la liste de handle par odre alphabétique.
+def alpha_order(arr)
+    arr
+        .sort_by{|w| w[1].match?(/[ascii]/)? 0 : 1}
+        .join(" ")
+end
+
+#Trie la liste de handle par taille des handle (du plus petit au plus grand)
+def length_order(arr)
+    return arr.sort_by(&:length)
+
+end
+#Quelle est la position dans l'array de la personne @epenser ?
+def index_finder(arr)
+    return arr.index("@epenser")
+end
+
+#Sors-moi une répartition des handle par taille de ces derniers (nombre de handle avec 1 caractère, nombre de handle avec 2 caractères, nombre de handle avec 3 caractères, etc)
+def partition_of_length(arr, nb_char)
+    count = 0
+    arr.each do |index|
+        if index.length == nb_char
+            count += 1
+        end
+    end
+    return count
+end
+
+# Menu of question # 
+def main_prog(array)
+    user_q = ""
+    while   user_q != "quit"
+        p "/" * 50 + "\\" * 50
+        puts    "    1.Combien y a-t'il de handle dans cette array ?
+    2.Quel est le handle le plus court de cette liste ?
+    3.Combien y-a-t'il de handle contenant 5 caractères ?(le @ ne compte pas pour un caractère)
+    4.Combien de handle commencent par une majuscule ? (première lettre juste après le @)
+    5.Trie la liste de handle par odre alphabétique.
+    6.Trie la liste de handle par taille des handle (du plus petit au plus grand)
+    7.Quelle est la position dans l'array de la personne @epenser ?
+    8.Sors-moi une répartition des handle par taille de ces derniers (nombre de handle avex 1 caractère, nombre de handle avec 2 caractères, nombre de handle avec 3 caractères, etc)\n\n"
+    
+        p "/" * 50 + "\\" * 50
+        puts    "Choisi un nombre correspondant à la question que tu te pose "
+        print   "> "
+        user_q = gets.chomp
+        case user_q
+        when "1"
+            puts "\nLe nombre de handle est de :  #{arr_length(array)} \n\n" 
+        when "2"
+            puts "\nLe handle le plus court de cette liste est #{smallest_string(array)} et est d'une taille de #{smallest_string(array).length} caractères.\n\n"
+        when "3"
+            puts "\n  Il y a #{find_string(array)} handle de 5 caractères !\n\n"
+        when "4"
+            puts "\n Il y a #{find_caps(array)} handle commencent par une majuscule !"
+        when "5"
+            puts "\n Le tableau à été trier par ordre alphabétique , pour afficher la modification avant / après appuyer sur 1, sinon n'importe quelle touche \n\n"
+            print "> "
+            put_array = gets.chomp.to_i 
+            if put_array == 1 then
+                puts "\n Avant le trie :\n #{array} \n\n Après le trie : \n #{alpha_order(array)}\n\n"
+            end
+        when "6"
+            puts "\n Le tableau à été trier par ordre croissant, pour afficher la modification avant / après appuyer sur 1, sinon, une autre touche enjoy =) \n\n"
+            print "> "
+            put_choice = gets.chomp.to_i
+            if put_choice == 1 then
+                puts "\n Avant le trie :\n #{array} \n\n Après le trie : \n #{length_order(array)}"
+            end
+        when "7"
+            puts "Voici la position de Mister @epenser : #{index_finder(array)}"
+        when "8"
+            puts "Selectionnez le nombre de caractères que vous voulez trouver dans le tableau "
+            print "> "
+            nb_of_char = gets.chomp.to_i
+            print "Voici vos résultat : pour #{nb_of_char} caractères il y a #{partition_of_length(array, nb_of_char)} handle ! \n\n"
+        when "quit"
+            puts "Merci d'avoir essayer !"  
+        else
+            puts "Merci d'utiliser les touches du menu comprise entre '1' et '8' ou 'quit' pour quitter !"
+        end
+    end
+end
+
+main_prog(array)
